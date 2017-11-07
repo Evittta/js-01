e40f62a95645a434e22bf8cb3cf95f2cbdc151d3
evita kusinya <vita.kusinya@gmail.com>
2017-11-07 15:40:57 +0000

    solution

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const a = Number(prompt('Enter a = '));
+const b = Number(prompt('Enter b = '));
+const c = Number(prompt('Enter c = '));
+
+function solveQuadr(a, b, c) {
+    let x1, x2;
+    const d = disc(a, b, c);
+    if (d > 0 ) {
+        x1 = (-b + Math.sqrt(d) ) / (2 * a);
+        x2 = (-b - Math.sqrt(d) ) / (2 * a);
+        return 'x1 = ' + x1 + ', x2 = ' + x2;
+    } else if (d < 0) {
+        return 'There are no real roots';
+    } else {
+        x1 = -b / (2 * a);
+        return 'x1 = ' + x1;
+    }
+}
+
+function disc(a, b, c) {
+    return b * b - 4 * a * c;
+}
+
+function print(val){
+    document.write(val);
+}
+
+const result = 'Solution of the equation : '+ solveQuadr(a, b, c);
+print(result);
+
+
+
+
+
+
+
+
+
